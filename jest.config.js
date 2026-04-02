30ca0918ef5e821846a4d7ef58caffdd7053303a
BrandonDevU <brandon.vargas1095@hotmail.com>
2026-04-02 16:19:02 -0600

    fix: remove unused preset in jest configuration and adjust tsconfig for isolated modules

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
 export default {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   collectCoverageFrom: ['src/**/*.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-    },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
 };
