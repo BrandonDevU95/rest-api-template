47db5a1d039232ab1949627f952679749cbbc1af
BrandonDevU <brandon.vargas1095@hotmail.com>
2026-04-03 13:13:46 -0600

    feat: add algorithm specification for JWT token signing

--- src/application/services/TokenService.ts
+++ src/application/services/TokenService.ts
@@ -12,12 +12,14 @@ export class TokenService {
   signAccessToken(payload: JwtPayload): string {
     return jwt.sign(payload, env.jwt.accessSecret, {
       expiresIn: env.jwt.accessExpiresIn as jwt.SignOptions['expiresIn'],
+      algorithm: 'HS256',
     });
   }
 
   signRefreshToken(payload: JwtPayload): string {
     return jwt.sign(payload, env.jwt.refreshSecret, {
       expiresIn: env.jwt.refreshExpiresIn as jwt.SignOptions['expiresIn'],
+      algorithm: 'HS256',
     });
   }
 
